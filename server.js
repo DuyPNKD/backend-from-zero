35c62c0569782ec86d54708f7a3ddca3c5c91bb4
DuyPNKD <165270115+DuyPNKD@users.noreply.github.com>
2024-12-17 14:07:52 +0700

    video 30

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express"); //import express
 const path = require("path");
 const configViewEngine = require("./src/config/viewEngine"); // import file config
 const webRoutes = require("./src/routes/web");
+const mysql = require("mysql2");
 
 const app = express(); // tạo express application
 const port = process.env.PORT || 8888; // init port
@@ -14,6 +15,22 @@ configViewEngine(app);
 
 // Khai báo routes
 app.use("/", webRoutes);
+
+//test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307, //default: 2206
+    user: "root", //default: empty
+    password: "123456",
+    database: "hoidanit",
+});
+
+// A simple SELECT query
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+    console.log(">>>>result: ", results); // results contains rows returned by server
+    console.log(">>>>fields: ", fields); // fields contains extra meta data about results, if available
+});
 
 // run server trên port đã khởi tạo trước đấy
 app.listen(port, hostname, () => {
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const {getHomePage, getABC, getHoiDanIT} = require("../controllers/homeController");
 
-router.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-router.get("/abc", (req, res) => {
-    res.send("Check AAAABBBBCCC!");
-});
-
-router.get("/hoidanit", (req, res) => {
-    // res.send("<h1>Hoi dan IT voi Eric</h1>");
-    res.render("sample.ejs");
-});
+router.get("/", getHomePage);
+router.get("/abc", getABC);
+router.get("/hoidanit", getHoiDanIT);
 
 module.exports = router;
