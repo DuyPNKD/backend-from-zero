d88b68e798bcfea99df6514ba5370c815fb9edf8
DuyPNKD <165270115+DuyPNKD@users.noreply.github.com>
2024-12-30 11:31:16 +0700

    86. Create a customer API

--- src/models/customer.js
+++ src/models/customer.js
@@ -14,5 +14,5 @@ const customerSchema = new mongoose.Schema(
     },
     {timestamps: true}
 );
-const Customer = mongoose.model("user", customerSchema);
+const Customer = mongoose.model("Customer", customerSchema);
 module.exports = Customer;
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routerAPI = express.Router();
 const {getUsersAPI, postCreateUserAPI, putUpdateUserAPI, deleteUserAPI, postUploadSingleFileAPI, postUploadMultipleFileAPI} = require("../controllers/apiController");
 
+const {postCreateCustomer} = require("../controllers/customerController");
 routerAPI.get("/", (req, res) => {
     res.send("Hello World with API");
 });
@@ -16,6 +17,10 @@ routerAPI.get("/users", getUsersAPI);
 routerAPI.post("/users", postCreateUserAPI);
 routerAPI.put("/users", putUpdateUserAPI);
 routerAPI.delete("/users", deleteUserAPI);
-routerAPI.post("/file", postUploadMultipleFileAPI);
+
+routerAPI.post("/file", postUploadSingleFileAPI);
+routerAPI.post("/files", postUploadMultipleFileAPI);
+
+routerAPI.post("/customers", postCreateCustomer);
 
 module.exports = routerAPI;
