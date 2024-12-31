c5f765bf644aabbe47c466790e19eba3a7995379
DuyPNKD <165270115+DuyPNKD@users.noreply.github.com>
2024-12-31 09:33:24 +0700

    100. Filter (dạng basic)

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -44,13 +44,14 @@ module.exports = {
         }
     },
     getAllCustomers: async (req, res) => {
-        console.log(req.query);
         let limit = req.query.limit;
         let page = req.query.page;
+        let name = req.query.name;
+
         let result = null;
 
         if (limit && page) {
-            result = await getAllCustomerService(limit, page);
+            result = await getAllCustomerService(limit, page, name);
         } else {
             result = await getAllCustomerService();
         }
